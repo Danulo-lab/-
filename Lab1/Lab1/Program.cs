c68dc681e35e34fcf47cd93a7a93df00ca8ea066
Данило <kalinikhenco04@gmail.com>
2025-08-29 13:39:02 +0300

    Add Lab1
    Додано лабораторна 1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+
+    namespace PowerLineApp
+    {
+        class PowerLineWire
+        {
+            public string Brand;
+            public double CrossArea;
+            public double Resistivity;
+            public string Material;
+
+            public PowerLineWire(string brand, double crossArea, double resistivity, string material)
+            {
+                Brand = brand;
+                CrossArea = crossArea;
+                Resistivity = resistivity;
+                Material = material;
+            }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Марка: {Brand}");
+                Console.WriteLine($"Площа перерізу: {CrossArea} мм²");
+                Console.WriteLine($"Питомий активний опір: {Resistivity} Ом·мм²/м");
+                Console.WriteLine($"Матеріал: {Material}");
+                Console.WriteLine(new string('-', 35));
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                PowerLineWire wire1 = new PowerLineWire("АС-70", 70, 0.0283, "Алюміній зі сталевим сердечником");
+                PowerLineWire wire2 = new PowerLineWire("М-25", 25, 0.0171, "Мідь");
+
+                wire1.PrintInfo();
+                wire2.PrintInfo();
+
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
