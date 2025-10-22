3a237a96912809025bf5cb5863f28b202f4174de
Данило <kalinikhenco04@gmail.com>
2025-10-22 13:15:22 +0300

    Add files via upload

---
+++ LR 2/LR 2/Wire.cs
@@ -0,0 +1,22 @@
+﻿class Wire
+{
+    private string marka;        
+    private double area;         
+    public double Resistivity;   
+    public string Material;      
+  
+    public void SetMarka(string m) => marka = m;
+    public string GetMarka() => marka;
+
+    public void SetArea(double a)
+    {
+        if (a > 0) area = a;
+    }
+    public double GetArea() => area;
+
+    
+    public override string ToString()
+    {
+        return $"Марка: {marka}, Площа: {area} мм², Опір: {Resistivity}, Матеріал: {Material}";
+    }
+}
